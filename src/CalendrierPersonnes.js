0d48a3a9ac509ad7a3a6ca02667925eda808ecf6
legrandBenjo <coupec.legrand@outlook.com>
2023-04-13 22:50:02 +0200

    Compléter les jours du mois restants jusqu'en décembre avec les noms des personnes

--- src/CalendrierPersonnes.js
+++ src/CalendrierPersonnes.js
@@ -1,47 +1,63 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { mois } from './components/Mois'
 import './CalendrierPersonnes.css';
 
-const CalendrierPersonnes = () => {
-  const [moisActuel, setMoisActuel] = useState(0);
+class CalendrierPersonnes extends Component {
+  constructor(props) {
+    super(props);
 
-  const moisPrecedent = () => {
-    setMoisActuel((moisActuel - 1 + mois.length) % mois.length);
+    const dateCourante = new Date();
+    const moisCourant = dateCourante.getMonth();
+    
+    this.state = {
+      moisActuel: moisCourant,
+    };
+  }
+
+  moisPrecedent = () => {
+    this.setState((prevState) => ({
+      moisActuel: (prevState.moisActuel - 1 + mois.length) % mois.length,
+    }));
   };
 
-  const moisSuivant = () => {
-    setMoisActuel((moisActuel + 1) % mois.length);
+  moisSuivant = () => {
+    this.setState((prevState) => ({
+      moisActuel: (prevState.moisActuel + 1) % mois.length,
+    }));
   };
 
-  const personnesMois = mois[moisActuel].personnes;
+  render() {
+    const { moisActuel } = this.state;
+    const personnesMois = mois[moisActuel].personnes;
 
-  return (
-    <div>
-      <div className="bg-light-grey dib br3 pa3 ma3 bw2 showdow-5">
-        <h2>{mois[moisActuel].nom}</h2>
-        <table>
-          <tbody>
-            {[0, 1, 2, 3].map((i) => (
-              <tr key={i}>
-                {personnesMois.slice(i * 8, (i + 1) * 8).map((personne, j) => (
-                  <td key={j}>
-                    <div className="enteTableau">{personne.nufiJour}</div><br/>
-                    <div className="f4 dark-red">{personne.jour}</div> <br />
-                    <div className="b">{personne.numero}</div> <br />
-                    <div className="">{personne.nom}</div>
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="ph3 mt4">
-          <div className="f6 link dim br-pill ba bw2 ph3 pv2 mb2 dib mid-green" onClick={moisPrecedent}>Mɑ̄ŋū Pēēsì</div>
-          <div className="f6 link dim br-pill ba bw2 ph3 pv2 mb2 dib dark-blue" onClick={moisSuivant}>Mɑ̄ŋū Ntāmbhì</div>
+    return (
+      <div>
+        <div className="bg-light-grey dib br3 pa3 ma3 bw2 showdow-5">
+          <h2>{mois[moisActuel].nom}</h2>
+          <table>
+            <tbody>
+              {[0, 1, 2, 3].map((i) => (
+                <tr key={i}>
+                  {personnesMois.slice(i * 8, (i + 1) * 8).map((personne, j) => (
+                    <td key={j}>
+                      <div className="enteTableau">{personne.nufiJour}</div><br/>
+                      <div className="f4 dark-red">{personne.jour}</div> <br />
+                      <div className="b">{personne.numero}</div> <br />
+                      <div className="">{personne.nom}</div>
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="ph3 mt4">
+            <div className="f6 link dim br-pill ba bw2 ph3 pv2 mb2 dib mid-green" onClick={this.moisPrecedent}>Mɑ̄ŋū Pēēsì</div>
+            <div className="f6 link dim br-pill ba bw2 ph3 pv2 mb2 dib dark-blue" onClick={this.moisSuivant}>Mɑ̄ŋū Ntāmbhì</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default CalendrierPersonnes;
