c08411d0c02896c4e10e75236702aa1bb344a8ec
legrandBenjo <coupec.legrand@outlook.com>
2023-07-06 08:43:55 +0200

    Réalignement de l'ordre des jour de marché

--- src/components/MarcheDuJour.js
+++ src/components/MarcheDuJour.js
@@ -1,11 +1,11 @@
 //MarcheDuJour.js
 const MarcheDuJour = [
-    {nufiJour: "Nsū'kwɑ̀", marche:"Baboné, Bafou Meya, Baham, Balatchi-Btos grand marché, Bana, Bandjoun, Bangam, Bazou, Dschang, Fondjomekwet, Kafeng" },
-    {nufiJour: "Nthʉ̄'ntāā", marche:"Bagam, Bakou, Baleng, Baleveng, Baloum, Bamendjou, Bandenkop, Bandrefam, bangou chefferie, Fotomena" },
-    {nufiJour: "Líé'nkwè’", marche:"Bafou chefferie, Bafouda, Bafoussam, Bangou ville, Batié, Djinso'o, Fongo Tongo, Foutouni 1" },
-    {nufiJour: "NKɑ́ɑ́tēē", marche:"Baham, Bandja, Bangang, Bangoua, Banka, Bansoa chefferie, Bayangam, Komako, Mbouda petit marché." },
+    {nufiJour: "Nsū'kwɑ̀", marche:"Baboné, Bana, Fondjomekwet, Bafou Meya, Baham, Balatchi-Btos grand marché,Bandjoun, Bangam, Bazou, Dschang, Kafeng" },
+    {nufiJour: "Nthʉ̄'ntāā", marche:" Baleveng, Baloum, Bagam, Bakou, Baleng, Bamendjou, Bandenkop, Bandrefam, bangou chefferie, Fotomena" },
+    {nufiJour: "Líé'nkwè’", marche:"Foutouni 1, Bafou chefferie, Bafouda, Bafoussam, Bangou ville, Batié, Djinso'o, Fongo Tongo" },
+    {nufiJour: "NKɑ́ɑ́tēē", marche:"Bandja,  Banka, Baham, Bangang, Bangoua, Bansoa chefferie, Bayangam, Komako, Mbouda petit marché." },
     {nufiJour: "Nzêngòò", marche:"Baham, Balatchi-Btos petit marché, Bandeng, Bandjoun, Batchingou" },
-    {nufiJour: "Ncômntēē", marche:"Babouantou, Bahouan, Bakassa, Baleveng, Bamena, Bangou carrefour, Bapa, Batoufanm, Dschang, Fomopea, Foutouni 2, Kombou, Nkometou" },
+    {nufiJour: "Ncômntēē", marche:"Babouantou,  Bakassa,  Foutouni 2,Baleveng, Bahouan, Bamena, Bangou carrefour, Bapa, Batoufanm, Dschang, Fomopea, Kombou, Nkometou" },
     {nufiJour: "Nzîngū", marche:"Bafang, Bafoussam grand marché, Balefock, Bati, Batié, Penka Michel, Toumaka" },
     {nufiJour: "Nzîsō", marche:"Baletet, Bameka, Bayangam, Mbouda Grand marché" },
 ];
